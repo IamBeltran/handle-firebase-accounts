4d18b930384e55311eecbcfceedf4bec5f238214
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2019-08-27 00:32:11 -0500

    style: add comment

--- system/windows/assets/js/windows/index.js
+++ system/windows/assets/js/windows/index.js
@@ -1,17 +1,28 @@
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ REQUIRE THIRDPARTY DEPENDENCIES MODULES.                                          │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
 const { ipcRenderer } = window.electron;
 
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │ TARGET EVENT LISTENERS                                                            │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+
+// » button with id btn-delete-user
 document.getElementById('btn-delete-user').addEventListener('click', () => {
   ipcRenderer.send('show-window-delete-user');
 });
 
+// » button with id btn-set-user
 document.getElementById('btn-set-user').addEventListener('click', () => {
   ipcRenderer.send('show-window-set-user');
 });
 
+// » button with id btn-add-user
 document.getElementById('btn-add-user').addEventListener('click', () => {
   ipcRenderer.send('show-window-add-user');
 });
 
+// » button with id btn-get-users
 document.getElementById('btn-get-users').addEventListener('click', () => {
   ipcRenderer.send('show-window-get-users');
 });
